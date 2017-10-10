b48e734be4e8c95266d675612061c62114f0f8dc
alogy <1129507496@qq.com>
2017-10-10 22:46:32 +0800

    change 首页路由

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,20 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+
+// 文章路由
+// 文章列表，添加文章，编辑文章，删除文章，文章详情
+Route::get('/list', '\App\Http/Constructors/article@index');
+Route::get('/list/{listId}', '\App\Http/Constructors/article@show');
+
+Route::get('/article/create', '\App\Http/Constructors/article@create');
+Route::post('/list', '\App\Http/Constructors/article@store');
+
+Route::get('/list/{listId}/edit', '\App\Http/Constructors/article@edit');
+Route::put('/list/{listId}', '\App\Http/Constructors/article@update');
+
+Route::get('/list/delete', '\App\Http/Constructors/article@delete');
